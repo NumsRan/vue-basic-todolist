b6a4fb634ef6a278d5304d8265a6b3b69035cfda
NumsRan <iavoran24@gmail.com>
2025-08-24 20:07:33 +0200

    refactor App: using directly v-model insted of v-on & v-bind && replace sort() by toSorted() because we don't need to change the originale todoList value

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,11 @@
     const hideCompleted = ref(false)
 
     // TODO APP FUNCTION
-    const filteredTask = computed(() => {
+    const sortedAndFilteredTask = computed(() => {
+        const sortedTask = todoList.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
         return hideCompleted.value
-        ? todoList.value.filter((todo) => todo.completed === false)
-        : todoList.value
+        ? sortedTask.filter((todo) => todo.completed === false)
+        : sortedTask
     })
 
     const createTask = () => {
@@ -27,11 +28,6 @@
         taskName.value = ''
     }
 
-    const updateTaskAndSort = (todo) => {
-        todo.completed = !todo.completed
-        todoList.value.sort((a, b) => a.completed > b.completed ? 1 : -1)
-    }
-
     const removeTask = (todoId) => {
         todoList.value = todoList.value.filter((todo) => todo.id !== todoId)
     }
@@ -42,23 +38,26 @@
 
     <form @submit.prevent="createTask">
         <input type="text" placeholder="New task to do" v-model="taskName" required>
-        <button>Create</button>
+        <button :disabled="taskName.length === 0">Create</button>
     </form>
 
-    <div @click="hideCompleted = !hideCompleted" class="pointer">
-        <input type="checkbox" :checked="hideCompleted" class="pointer">
-        <span>{{ hideCompleted? 'Show all tasks' : 'Hide completed tasks' }}</span>
+    <div>
+        <input id="toggleState" type="checkbox" v-model="hideCompleted">
+        <label for="toggleState">
+            {{ hideCompleted? 'Show all tasks' : 'Hide completed tasks' }}
+        </label>
     </div>
 
-    <div v-if="filteredTask.length === 0">No task created...</div>
-
-    <ul>
-        <li v-for="todo in filteredTask" :key="todo.id">
-            <input type="checkbox" @click="updateTaskAndSort(todo)" :checked="todo.completed">
-            <span :class="{completed: todo.completed}">{{ todo.title }}</span>
-            <button @click="removeTask(todo.id)">Delete</button>
-        </li>
-    </ul>
+    <div v-if="sortedAndFilteredTask.length === 0">No task created...</div>
+    <div v-else>
+        <ul>
+            <li v-for="todo in sortedAndFilteredTask" :key="todo.id">
+                <input type="checkbox" v-model="todo.completed">
+                <span :class="{completed: todo.completed}">{{ todo.title }}</span>
+                <button @click="removeTask(todo.id)">Delete</button>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
