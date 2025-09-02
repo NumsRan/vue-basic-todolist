a881c3942e7233685684f7846e60de992207e6af
NumsRan <iavoran24@gmail.com>
2025-09-02 20:09:35 +0200

    feat App: init bootstrap and bootstrap-icon packages on the project

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 import { createApp } from "vue";
 import App from "./App.vue";
 
